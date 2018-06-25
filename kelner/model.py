258e37b277326f3d56393e449517c987bb423d89
Leszek <367876+lunardog@users.noreply.github.com>
2018-06-25 09:26:28 +0900

    small fix to model loading

--- kelner/model.py
+++ kelner/model.py
@@ -2,9 +2,11 @@ from .models import kelner_model, KerasModel, TensorflowModel
 
 
 def load(model_file_name, engine="keras", *args, **kwargs):
-    m = None
+    """ Loads a saved model, given engine """
     if engine == "keras":
         m = KerasModel(model_file_name, *args, **kwargs)
     elif engine == "tensorflow" or engine == "tf":
         m = TensorflowModel(model_file_name, *args, **kwargs)
+    else:
+        raise ValueError("The engine param must be one of [keras, tensorflow, tf]")
     return m
